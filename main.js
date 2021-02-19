b74982ef47f4f550beb424a82dabbf136b1c52b2
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2021-02-19 05:58:57 +0300

    Read input args from terminal to determine wether players move together or separately. Implemented HuntingGame class that runs the game based on the conditions provided by the user. Added more conditions for hunting/resting when a player starts the game.

--- main.js
+++ main.js
@@ -25,11 +25,12 @@ class Node {
   }
 
   hunt(){
+    // When a node is hunted, reduce the number of boars at the node.
     this.boars = this.boars - 1;
   }
 }
 
-class Paths {
+class PathGenerator {
 
   constructor (hunting_map) {
     this.hunting_map = hunting_map;
@@ -37,11 +38,19 @@ class Paths {
     this.keys = Object.keys(hunting_map);
     this.nodes = {};
 
-    this.setNodes();
-    this.setNodePathsArray();
+    this._setNodes();
+    this._setPathsArray();
+    this._setNodePathsArray();
   }
 
-  setPathsArray () {
+  _setNodes () {
+    // Create Node instances for each point on the map.
+    this.keys.forEach((key, index) => {
+      this.nodes[key] = new Node(index, key);
+    });
+  }
+
+  _setPathsArray () {
     this.keys.forEach(key => {
       var parents = [];
       // Check if the current key has parents in the hunting map.
@@ -56,26 +65,14 @@ class Paths {
         }
       });
     })
-    // Filter out all paths that do not end in 'K'.
+    // Filter out all paths that do not end in 'K' as they are incomplete.
     this.paths = this.paths.filter(path => (path[path.length - 1] === 'K'));
   }
 
-  getPathsArray () {
-    return this.paths;
-  }
-
-  setNodes () {
-    this.keys.forEach((key, index) => {
-      this.nodes[key] = new Node(index, key);
-    });
-  }
-
-  getNodes () {
-    return this.nodes();
-  }
-
-  setNodePathsArray () {
-    this.setPathsArray();
+  _setNodePathsArray () {
+    // Generate the same paths that are generated in _setPathsArray,
+    // but instead of each point being a simple label, the points
+    // are represented by nodes with more information.
     this.node_paths = this.paths.map(path => {
       var node_path = []
       path.forEach(point => {
@@ -83,7 +80,14 @@ class Paths {
       })
       return node_path
     })
-    // console.log(this.node_paths);
+  }
+
+  getPathsArray () {
+    return this.paths;
+  }
+
+  getNodes () {
+    return this.nodes();
   }
 
   getNodePathsArray () {
@@ -91,6 +95,10 @@ class Paths {
   }
 
   getOptimumPath () {
+    // Get the optimum path to maximise prey.
+    // The Optimum path will be the path with the most possible boars.
+    // This method is called by the player to determine which path they should take
+    // before starting.
     let optimum_path = {
       total_boars: 0,
       node_path: []
@@ -111,19 +119,105 @@ class Paths {
   }
 }
 
-var p = new Paths(hunting_map);
-console.log(p.getOptimumPath());
-
 class Player {
   
-  constructor (name) {
+  constructor (name, pathGenerator) {
     this.name = name;
+    this.pathGenerator = pathGenerator;
     this.stamina = 3;
     this.prey = 0;
     this.path = [];
+    this.currentNodeIndex = 0;
+
+    this._establishPath();
   }
 
+  _establishPath () {
+    this.path = this.pathGenerator.getOptimumPath();
+  }
 
+  _hunt () {
+    this.stamina -= 1;
+    this.prey += 1;
+    this.path[this.currentNodeIndex].hunt();
+    this._moveToNextNode();
+  }
+
+  _rest () {
+    this.stamina += 2;
+    if (this.stamina > 3) this.stamina = 3;
+    this._moveToNextNode();
+  }
+
+  _moveToNextNode () {
+    this.stamina -= 1;
+    this.currentNodeIndex += 1;
+  }
+
+  _printResult () {
+    console.log(this.prey);
+    var pathString = '';
+    this.path.forEach(node => {
+      pathString += node.name + ' ';
+    })
+    console.log(pathString);
+  }
+
+  start () {
+    while(this.currentNodeIndex < this.path.length) {
+      // If the player has enough stamina to hund AND move, hunt
+      // Otherwise, rest
+      // However, if we're on the last node (K in this case), the player has any 
+      // stamina remaining, and the node has any boars remaining, 
+      // hunt as there will be no more movement required.
+      if (
+        (this.currentNodeIndex === this.path.length - 1) &&
+        this.stamina &&
+        this.path[this.currentNodeIndex].getBoars()
+      ) {
+        this._hunt();
+      } else if (
+        this.stamina >= 2 &&
+        this.path[this.currentNodeIndex].getBoars()
+      ){
+        this._hunt();
+      } else {
+        this._rest();
+      }
+      
+    }
+    this._printResult();
+  }
 }
 
-// console.log(optimum_path);
+class HuntingGame {
+  constructor (args, hunting_map) {
+    this.players = {
+      '-t': [{
+        name: 'Dutch & Dylan (D&D)'
+      }],
+      '-s': [
+        {
+          name: 'Dutch'
+        },
+        {
+          name: 'Dylan'
+        }
+      ]
+    }
+    this.arg = args.length ? args[0] : '-t';
+    this.pathGenerator = new PathGenerator(hunting_map);
+  }
+
+  play () {
+    this.players[this.arg].forEach(player => {
+      new Player(player.name, this.pathGenerator).start();
+    })
+  }
+}
+
+const args = process.argv.slice(2);
+
+var game = new HuntingGame(args, hunting_map);
+game.play();
+
