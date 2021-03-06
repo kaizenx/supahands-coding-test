3c59c3afaeb526a5248966393cdd54bd9923a021
sw <tansw98@gmail.com>
2021-03-06 16:03:37 +0800

    add solution for coding test

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from classes import Hunter, Jungle, Simulator
+import argparse
+
+def init_parser():
+  parser = argparse.ArgumentParser(description='Hunting simulator for Supahands coding test.')
+  parser.add_argument(
+    '--json',
+    metavar='path',
+    type=str,
+    const='dump.json',
+    nargs='?',
+    help='dump results into a json file',
+  )
+  return parser
+
 def main():
-  prey = 0
   hunting_map = {
     'A':['B','C','K'],
     'B':['D','E'],
@@ -13,7 +27,18 @@ def main():
     'J':['K'],
     'K':[]
   }
-  print(hunting_map)
-  print(prey)
+  start_node = 'A'
+  end_node = 'K'
 
-main()
+  parser = init_parser()
+  args = parser.parse_args()
+
+  dutch = Hunter('Dutch', start_node=start_node)
+  dylan = Hunter('Dylan', start_node=start_node)
+  jungle = Jungle(hunting_map, start_node=start_node, end_node=end_node)
+  simulator = Simulator([dutch, dylan], jungle, args.json)
+
+  simulator.run()
+
+if __name__ == '__main__':
+  main()
